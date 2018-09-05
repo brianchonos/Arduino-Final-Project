4d16424fccc5c3dfced0c5a68105dfe0eb1388e6
Brian Chonos <bkc50717@gmail.com>
2018-09-05 12:32:04 -0700

    Add files via upload
    This is the code for the mic project

---
+++ 4b. our_own/Our_own/Our_own.ino
@@ -0,0 +1,44 @@
+const int num = 6;
+const int kpinleds[num]={2,3,4,5,6,7};
+void setup() {
+  for(int k=0;k<num;k++)
+  {
+    pinMode(kpinleds[k],OUTPUT);
+  }
+  pinMode(10,INPUT);
+  pinMode(10,HIGH);
+}
+
+void loop() {
+if (digitalRead(10)==LOW)
+{
+   for(int i=0;i<num;i++)
+  {
+    digitalWrite(kpinleds[i],LOW);
+    delay(100);
+    digitalWrite(kpinleds[i],HIGH);
+  }
+  for(int x=num-1;x>=0;x--)
+  {
+    digitalWrite(kpinleds[x],LOW);
+    delay(100);
+    digitalWrite(kpinleds[x],HIGH);
+  }
+}
+else
+{
+  for(int i=0;i<num;i++)
+  {
+    digitalWrite(kpinleds[i],HIGH);
+    delay(100);
+    digitalWrite(kpinleds[i],LOW);
+  }
+  for(int x=num-1;x>=0;x--)
+  {
+    digitalWrite(kpinleds[x],LOW);
+    delay(100);
+    digitalWrite(kpinleds[x],HIGH);
+  }
+}
+
+}
